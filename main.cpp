072cdc270f8c897501b371ab6757a74de2c124df
mr.morganfrimen@yandex.ru <Sergey>
2024-05-16 12:37:18 +0300

    mutable

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ class Entity
 private:
 	int m_X, m_Y;
     int var;
+	std::string m_Name;
+    int m_Debug_Cout = 0;
 public:
 	int GetX() const
 	{
@@ -20,11 +22,31 @@ public:
 	{
 		m_X = x;
 	}
+
+	const std::string& GetName() const 
+	{ 
+		return m_Name; 
+	}
+	std::string& GetName()
+	{
+		m_Debug_Cout++;
+		return m_Name;
+	}
+
+	void SetName(std::string name)
+	{
+		m_Name = name;
+	}
+
 };
 
 void PrintEntity(const Entity& e)
 {
 	std::cout << e.GetX() << std::endl;
+}
+void PrintName(const Entity& name)
+{
+	std::cout << name.GetName() << std::endl;
 }
 
 int main()
@@ -32,6 +54,20 @@ int main()
 	Entity e;
 	e.SetX(55);
 	PrintEntity(e);
+
+	Entity name;
+	name.SetName("Hello");
+	PrintName(name);
+
+	int x = 8;
+	auto f = [=]() mutable
+		{
+			x++;
+			std::cout << x << std::endl;
+		};
+
+	f();
+
 
 	const int MAX_AGE = 90;
 	const int* a = new int;
